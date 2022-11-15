b09d47558a1633367a21aaaaf539715d343b8ccd
Serhii <ex@example.com>
2022-11-15 21:59:11 +0200

    add view artical, model controllers

--- resources/views/blogs/inc/blogs/realetedSection.blade.php
+++ resources/views/blogs/inc/blogs/realetedSection.blade.php
@@ -2,17 +2,17 @@
     <h2 class="section-title mb-4" data-aos="fade-up">Related Posts</h2>
     <div class="row">
         <div class="col-md-4" data-aos="fade-right" data-aos-delay="100">
-            <img src="{{asset('/images/blog_post_related_1.png')}}" alt="related post" class="post-thumbnail">
+            <img src="{{asset('assets/images/blog_post_related_1.png')}}" alt="related post" class="post-thumbnail">
             <p class="post-category">Blog post</p>
             <h5 class="post-title">Front becomes an official Instagram</h5>
         </div>
         <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
-            <img src="{{asset('/images/blog_post_related_2.png')}}" alt="related post" class="post-thumbnail">
+            <img src="{{asset('assets/images/blog_post_related_2.png')}}" alt="related post" class="post-thumbnail">
             <p class="post-category">Blog post</p>
             <h5 class="post-title">Front becomes an official Instagram</h5>
         </div>
         <div class="col-md-4" data-aos="fade-left" data-aos-delay="100">
-            <img src="{{asset('/images/blog_post_related_3.png')}}" alt="related post" class="post-thumbnail">
+            <img src="{{asset('assets/images/blog_post_related_3.png')}}" alt="related post" class="post-thumbnail">
             <p class="post-category">Blog post</p>
             <h5 class="post-title">Front becomes an official Instagram</h5>
         </div>
